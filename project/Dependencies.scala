1b431b14453788379023c6043bc20f9e5e59198d
Anand Singh <imanandksingh@gmail.com>
2021-12-18 16:12:56 +0000

    Update scalatest to 3.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
     }
 
     case object scalatest {
-      val scalatest = "org.scalatest" %% "scalatest" % "3.2.9"
+      val scalatest = "org.scalatest" %% "scalatest" % "3.2.10"
     }
 
     case object scalatestplus {
