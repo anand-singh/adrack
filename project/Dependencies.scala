99c66b0a43b10e5ddef3685eaae3065585a34aac
Anand Singh <4903911+anand-singh@users.noreply.github.com>
2022-09-20 08:58:03 +0000

    Update scalacheck from 1.16.0 to 1.17.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
     }
 
     case object scalacheck {
-      val scalacheck = "org.scalacheck" %% "scalacheck" % "1.16.0"
+      val scalacheck = "org.scalacheck" %% "scalacheck" % "1.17.0"
     }
 
     case object scalatest {
